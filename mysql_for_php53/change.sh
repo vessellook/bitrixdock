8399aea877f5c2385a3a4daa1b6200bb5538b10c
vessellook <vessellook@gmail.com>
2020-11-09 21:09:41 +0300

    Fix in bash script
    Instructions for PHP-5.3 configuration:
    - Run `docker-compose up`
    - Get db container name via `docker ps`. Look for name like PROJECTdb (PROJECT from .env file)
    - Enter db container via `docker exec -it <container name> bash`
    - Run command `./change.sh ${MYSQL_USER} ${MYSQL_PASSWORD} ${MYSQL_ROOT_PASSWORD}`
    - Enter root password for mysql in prompt
    - Exit container (command `exit`)

--- mysql_for_php53/change.sh
+++ mysql_for_php53/change.sh
@@ -1,1 +1,1 @@
-mysql -u 'root'@'localhost' -p"$3" -e "ALTER USER '$1' IDENTIFIED WITH mysql_native_password BY '$2';"
+mysql -u 'root' -p"$3" -e "ALTER USER '$1' IDENTIFIED WITH mysql_native_password BY '$2';"
